6b3398fb090219e8a90845740175f0510586d471
ashab96 <asha99phy@gmail.com>
2023-01-08 16:00:03 +0530

    log removing

--- components/HomeMain/HomeHero/HomeHero.js
+++ components/HomeMain/HomeHero/HomeHero.js
@@ -2,7 +2,6 @@ import React from "react";
 import { urlFor } from "../../../lib/client";
 
 const HomeHero = ({ homepage }) => {
-  console.log(homepage);
   return (
     <div className="relative">
       <div className="text-center md:text-left md:absolute md:font-brandon font-[1000] text-[32px] md:text-[48px] text-black-300 md:text-black-10 leading-[2.5rem] md:leading-[3.75rem] md:pl-16 py-4 md:w-3/4 lg:w-3/4 flex flex-col flex-nowrap items-start justify-center md:bg-gradient-to-r from-black-900 to-black-10/0 md:top-0 md:bottom-0 left-0">
